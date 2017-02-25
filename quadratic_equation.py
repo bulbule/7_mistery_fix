4ae43723be4fbd48d5cdeed3f9713301e3b90c3f
bulbule <anyagoremykina@gmail.com>
2017-02-25 22:31:52 +0100

    return None for negative discriminant

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,11 +1,15 @@
-from math import sqrt
+import math
 
 
 def get_roots(a, b, c):
+
     discriminant = b ** 2 - 4 * a * c
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
+        root1 = -b / (2 * a)
         return root1, None
-    else:
+    elif discriminant > 0:
+        root1 = (-b - math.sqrt(discriminant)) / (2 * a)
+        root2 = (-b + math.sqrt(discriminant)) / (2 * a)
         return root1, root2
+    else:
+        return None, None
